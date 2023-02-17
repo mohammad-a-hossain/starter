7795936a535a9fe2349e008b05adc49e27cff5b9
mohammad-a-hossain <adam.eve007@gmail.com>
2023-02-17 14:26:15 +0600

    try

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   content: [
     '**/*.html'],
-  // ...
+  darkMode:"media", 
 }
 
 
 // Remove Line: purge: [], darkMode: false
 // Add Line: content: ["./*.{html,js}"], darkMode: 'class'
+
+//
+//module.exports = {
+ // content: [
+ //   '**/*.html'],
+  // ...
+//}
